7eeb6c9f5174df9121e1362718c3578480168085
chenpeiqing1231 <cpeiqing@astreetpress.com>
2020-04-06 22:25:20 +0800

    fix style

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -1,32 +1,87 @@
 <template>
-  <div class="home-banner">
-    <search></search>
-<!--    <ul class="slides">-->
-<!--      <li>-->
-<!--        <picture>-->
-<!--          <source :srcset="require('@/assets/home/keyvisual_homepage_mega.png')" media="(min-width: 1600px)">-->
-<!--          <source :srcset="require('@/assets/home/keyvisual_homepage_tiny.jpg')" media="(min-width: 1024px)">-->
-<!--          <source :srcset="require('@/assets/home/keyvisual_homepage_small.jpg')" media="(min-width: 768px)">-->
-<!--          <source :srcset="require('@/assets/home/keyvisual_homepage_medium.jpg')" media="(min-width: 640px)">-->
-<!--          <source :srcset="require('@/assets/home/keyvisual_homepage_big.jpg')">-->
-<!--          <img-->
-<!--            :src="require('@/assets/home/keyvisual_homepage_mega.png')"-->
-<!--            :srcset="require('@/assets/home/keyvisual_homepage_tiny.jpg') + ' 480w,'-->
-<!--      + require('@/assets/home/keyvisual_homepage_small.jpg') + ' 640w,'-->
-<!--      + require('@/assets/home/keyvisual_homepage_medium.jpg') + ' 768w,'-->
-<!--      + require('@/assets/home/keyvisual_homepage_big.jpg') + ' 1024w,'-->
-<!--      + require('@/assets/home/keyvisual_homepage_mega.png') + ' 1600w'"-->
-<!--            alt="Stage"-->
-<!--          >-->
-<!--        </picture>-->
-<!--      </li>-->
-<!--    </ul>-->
-  </div>
+  <v-container
+    tag="section"
+    fluid
+  >
+    <v-row>
+      <v-col cols="12" md="6" sm="6">
+        <div
+          :align="alignment"
+          :justify="justify"
+          class="search-container"
+          style="height: 320px;"
+        >
+            <span>
+              欢迎访问巴斯夫分散体与树脂产品中心！
+在这里，您可以轻松查找您所需的巴斯夫产品信息，<a href="">注册</a>用户更可浏览详细的应用配方或同我们的技术专家深入交流。在线样品定购和1688官方旗舰店下单等便捷功能可随时满足您的采购需求。
+            </span>
+          <search></search>
+        </div>
+      </v-col>
+      <v-col cols="12" md="6" sm="6">
+        <div
+          :align="alignment"
+          :justify="justify"
+          class="teaser-container orange1"
+          style="height: 320px;"
+        >
+          <a href="https://www.basf.com/global/en/corona-information.html" target="_blank">
+            <picture target="_blank" href="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_corona.jpg?sfvrsn=e211d160_6">
+              <!--[if IE 9]><video style="display: none;"><![endif]-->
+              <source srcset="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_corona.jpg?sfvrsn=e211d160_6" media="(min-width: 1024px)">
+              <source srcset="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_corona.jpg?sfvrsn=e211d160_6">
+              <!--[if IE 9]></video><![endif]-->
+              <img src="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_corona.jpg?sfvrsn=e211d160_6" srcset="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_corona.jpg?sfvrsn=e211d160_6 1279w, https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_corona.jpg?sfvrsn=e211d160_6 1024w" alt="home_teaser_corona">
+            </picture>
+          </a>
+
+          <div class="caption">
+            <a href="https://www.basf.com/global/en/corona-information.html" target="_blank">
+              <h1>What’s new</h1>
+              <h2>Coronavirus and Measures at BASF</h2>
+            </a>
+
+            <p><strong>The novel coronavirus COVID-19 pandemic poses unprecedented challenges for all of us.
+            </strong><br> The well-being of our employees and supplying our customers in the best possible way have the utmost priority for us.</p>
+
+            <a href="https://www.basf.com/global/en/corona-information.html" target="_blank" class="external">Read More</a>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!--    <ul class="slides">-->
+  <!--      <li>-->
+  <!--        <picture>-->
+  <!--          <source :srcset="require('@/assets/home/keyvisual_homepage_mega.png')" media="(min-width: 1600px)">-->
+  <!--          <source :srcset="require('@/assets/home/keyvisual_homepage_tiny.jpg')" media="(min-width: 1024px)">-->
+  <!--          <source :srcset="require('@/assets/home/keyvisual_homepage_small.jpg')" media="(min-width: 768px)">-->
+  <!--          <source :srcset="require('@/assets/home/keyvisual_homepage_medium.jpg')" media="(min-width: 640px)">-->
+  <!--          <source :srcset="require('@/assets/home/keyvisual_homepage_big.jpg')">-->
+  <!--          <img-->
+  <!--            :src="require('@/assets/home/keyvisual_homepage_mega.png')"-->
+  <!--            :srcset="require('@/assets/home/keyvisual_homepage_tiny.jpg') + ' 480w,'-->
+  <!--      + require('@/assets/home/keyvisual_homepage_small.jpg') + ' 640w,'-->
+  <!--      + require('@/assets/home/keyvisual_homepage_medium.jpg') + ' 768w,'-->
+  <!--      + require('@/assets/home/keyvisual_homepage_big.jpg') + ' 1024w,'-->
+  <!--      + require('@/assets/home/keyvisual_homepage_mega.png') + ' 1600w'"-->
+  <!--            alt="Stage"-->
+  <!--          >-->
+  <!--        </picture>-->
+  <!--      </li>-->
+  <!--    </ul>-->
 </template>
 
 <script>
   export default {
     name: 'Banner',
+
+    data () {
+      return {
+        alignment: 'center',
+        justify: 'center',
+      }
+    },
 
     components: {
       Search: () => import('@/components/home/Search')
@@ -52,5 +107,22 @@
 
   .home-banner {
     margin-top: 45px;
+  }
+
+  .teaser-container {
+    
+    &.orange1 {
+      border-color: #f49800;
+    }
+  }
+
+  .search-container {
+    height: 320px;
+    overflow: hidden;
+    position: relative;
+    text-align: left;
+    padding: 24px 30px 240px !important;
+    background-color: #fff;
+    width: 98.555555%;
   }
 </style>
--- src/components/home/Features.vue
+++ src/components/home/Features.vue
@@ -1,49 +1,77 @@
 <template>
   <v-container
-    id="features"
+    fluid
     tag="section"
     class="bounceUp-enter-active"
   >
-    <v-row dense>
-      <v-col cols="12" md="3" sm="6" class="features-card" v-for="(featureProduct, index) in featureProducts" :key="index">
+    <v-row>
+      <v-col cols="12" md="6" sm="6" v-for="(featureProduct, index) in featureProducts" :key="index">
         <v-hover v-slot:default="{ hover }">
-          <v-card
-            class="mx-auto features-product"
-            max-width="500"
-            :elevation="hover ? 18 : 2"
-          >
-            <v-card-title>
-              <template v-if="index === 0">
-                <icon-features1 width="100" height="100"></icon-features1>
-              </template>
+<!--          <v-card-->
+<!--            class="mx-auto features-product"-->
+<!--            max-width="500"-->
+<!--            :elevation="hover ? 18 : 2"-->
+<!--          >-->
+<!--            <v-card-title>-->
+<!--              <template v-if="index === 0">-->
+<!--                <icon-features1 width="100" height="100"></icon-features1>-->
+<!--              </template>-->
 
-              <template v-else-if="index === 1">
-                <icon-features2 width="100" height="100"></icon-features2>
-              </template>
+<!--              <template v-else-if="index === 1">-->
+<!--                <icon-features2 width="100" height="100"></icon-features2>-->
+<!--              </template>-->
 
-              <template v-else-if="index === 2">
-                <icon-features3 width="100" height="100"></icon-features3>
-              </template>
+<!--              <template v-else-if="index === 2">-->
+<!--                <icon-features3 width="100" height="100"></icon-features3>-->
+<!--              </template>-->
 
-              <template v-else-if="index === 3">
-                <icon-features4 width="100" height="100"></icon-features4>
-              </template>
-              <span class="title font-weight-light"><samll>Product <br> Highlight</samll></span>
-            </v-card-title>
+<!--              <template v-else-if="index === 3">-->
+<!--                <icon-features4 width="100" height="100"></icon-features4>-->
+<!--              </template>-->
+<!--              <span class="title font-weight-light"><samll>Product <br> Highlight</samll></span>-->
+<!--            </v-card-title>-->
 
-            <v-card-actions class="title-name">
-              <v-list-item class="grow">
-                <h1>
-                  <router-link :to="{name: 'Product', params: {id: featureProduct.uuid}}">
-                    {{ featureProduct.title }}<v-icon>keyboard_arrow_right</v-icon>
-                  </router-link>
-                </h1>
-              </v-list-item>
-            </v-card-actions>
+<!--            <v-card-actions class="title-name">-->
+<!--              <v-list-item class="grow">-->
+<!--                <h1>-->
+<!--                  <router-link :to="{name: 'Product', params: {id: featureProduct.uuid}}">-->
+<!--                    {{ featureProduct.title }}<v-icon>keyboard_arrow_right</v-icon>-->
+<!--                  </router-link>-->
+<!--                </h1>-->
+<!--              </v-list-item>-->
+<!--            </v-card-actions>-->
 
-            <v-card-text class="subtitle-1" v-html="featureProduct.body">
-            </v-card-text>
-          </v-card>
+<!--            <v-card-text class="subtitle-1" v-html="featureProduct.body">-->
+<!--            </v-card-text>-->
+<!--          </v-card>-->
+          <div class="teaser-container orange1" :elevation="hover ? 18 : 2">
+            <a href="https://carecreations.basf.us/stories/covid-19-march-2020" target="_blank">
+              <picture target="_blank" href="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_hands.jpg?sfvrsn=76c5d260_2">
+                <!--[if IE 9]><video style="display: none;"><![endif]-->
+                <source srcset="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_hands.jpg?sfvrsn=76c5d260_2" media="(min-width: 1024px)">
+                <source srcset="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_hands.jpg?sfvrsn=76c5d260_2">
+                <!--[if IE 9]></video><![endif]-->
+                <img src="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_hands.jpg?sfvrsn=76c5d260_2" srcset="https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_hands.jpg?sfvrsn=76c5d260_2 1279w, https://www.carecreations.basf.com/images/default-source/homepage/teaser/2020/home_teaser_hands.jpg?sfvrsn=76c5d260_2 1024w" alt="home_teaser_hands">
+              </picture>
+            </a>
+
+            <div class="caption">
+              <a href="https://carecreations.basf.us/stories/covid-19-march-2020" target="_blank">
+                <h1>What’s new</h1>
+                <h2>News from North America</h2>
+              </a>
+
+              <p><strong>Hand Hygiene Formulation Support in Response to COVID-19
+              </strong><br> While consumers practice social distancing, manufacturers are
+                hard at work supplying the goods needed to stay safe and healthy
+                while the virus runs its course. People especially need liquid hand soaps,
+                hand sanitizers, wipes, and similar products to keep their bodies and
+                environments free of pathogens.
+              </p>
+
+              <a href="https://carecreations.basf.us/stories/covid-19-march-2020" target="_blank" class="external">Read More</a>
+            </div>
+          </div>
         </v-hover>
       </v-col>
     </v-row>
@@ -52,14 +80,14 @@
 
 <script>
   // Utilities
-  import IconFeatures1 from '../svg/features/Features-1'
-  import IconFeatures2 from '../svg/features/Features-2'
-  import IconFeatures3 from '../svg/features/Features-3'
-  import IconFeatures4 from '../svg/features/Features-4'
+  // import IconFeatures1 from '../svg/features/Features-1'
+  // import IconFeatures2 from '../svg/features/Features-2'
+  // import IconFeatures3 from '../svg/features/Features-3'
+  // import IconFeatures4 from '../svg/features/Features-4'
   import { mapState } from 'vuex'
 
   export default {
-    components: { IconFeatures1, IconFeatures2, IconFeatures3, IconFeatures4 },
+    components: { },
 
     data: function () {
       return {
@@ -88,13 +116,8 @@
 
 <style lang="scss" scoped>
   #features {
-    //margin-top: -45%;
-    max-width: 100%;
-    min-width: 100%;
-    padding: 4px;
 
     & > div {
-      max-width: 1070px;
       margin: 0 auto;
     }
 
